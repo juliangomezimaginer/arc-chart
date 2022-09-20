da1509e2c4c4f4cbe8122fac622e8118e38b1640
Julian Gomez <jgomez109@unab.edu.co>
2022-09-20 17:38:38 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArcComponent } from './components/arc/arc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArcComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/arc/arc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arc',
+  templateUrl: './arc.component.html',
+  styleUrls: ['./arc.component.scss']
+})
+export class ArcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
