5e6d75ad48b9f3720283a74b7c5a463ccc80759b
Julian Gomez <jgomez109@unab.edu.co>
2022-09-21 11:29:37 -0500

    Add conditions

--- src/app/components/arc/arc.component.ts
+++ src/app/components/arc/arc.component.ts
@@ -8,23 +8,89 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ArcComponent implements OnInit {
   @Input() critical: number = 10;
   @Input() low: number = 15;
-  @Input() full: number = 10;
+  @Input() full: number = 50;
   @Input() current: number = 50;
   @Input() unit: string = 'cans';
 
-  get criticalDotValue(): string {
-    return this.updateCriticalDotValue();
+  circleFull = 0;
+  circleLow = 0;
+  circleCritical = 0;
+
+  private _lastCriticalPercentage = 0;
+  private _lastLowPercentage = 0;
+  private _lastFullPercentage = 0;
+  private _lastCurrentPercentage = 0;
+
+  get criticalDotPercentage(): string {
+    return `${Math.round(this.updateCriticalPercentage()).toLocaleString()}%`;
   }
 
-  get lowDotValue(): string {
-    return `${(this.low * 100) / this.current}%`;
+  get lowDotPercentage(): string {
+    return `${Math.round(this.updateLowPercentage()).toLocaleString()}%`;
+  }
+
+  get currentChartValue(): string {
+    return `${Math.round(this.updateCurrentPercentage()).toLocaleString()}%`;
   }
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  updateCriticalDotValue(): string {
-    return `${(this.critical * 100) / this.current}%`;
+  updateChart(): void {
+    const critical = this._lastCriticalPercentage;
+    const low = this._lastLowPercentage;
+    const current = this._lastCurrentPercentage;
+    if (current < critical) {
+      this.circleFull = 0;
+      this.circleLow = 0;
+      this.circleCritical = Math.round(current);
+    } else if (critical <= current && current < low) {
+      this.circleFull = 0;
+      this.circleLow = Math.round(current);
+      this.circleCritical = Math.round(critical);
+    } else {
+      this.circleFull = Math.round(current);
+      this.circleLow = Math.round(low);
+      this.circleCritical = Math.round(critical);
+    }
+    const criticalDot = Math.round(critical);
+    const lowDot = Math.round(low);
+  }
+
+  updateCurrentPercentage(): number {
+    let value = 0;
+    if (this.full > 0) {
+      value = (this.current * 100) / this.full;
+    }
+    if (value > 100) value = 100;
+    if (value < 0) value = 0;
+    this._lastCurrentPercentage = value;
+    this.updateChart();
+    return value;
+  }
+
+  updateCriticalPercentage(): number {
+    let value = 0;
+    if (this.full > 0) {
+      value = (this.critical * 100) / this.full;
+    }
+    if (value > 100) value = 100;
+    if (value < 0) value = 0;
+    this._lastCriticalPercentage = value;
+    this.updateChart();
+    return value;
+  }
+
+  updateLowPercentage(): number {
+    let value = 0;
+    if (this.full > 0) {
+      value = (this.low * 100) / this.full;
+    }
+    if (value > 100) value = 100;
+    if (value < 0) value = 0;
+    this._lastLowPercentage = value;
+    this.updateChart();
+    return value;
   }
 }
