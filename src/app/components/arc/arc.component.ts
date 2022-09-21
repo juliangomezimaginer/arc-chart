bf95bcbf998d7b9ff810ae5b1a019067b8f28bc6
Julian Gomez <jgomez109@unab.edu.co>
2022-09-21 10:40:23 -0500

    Add inputs

--- src/app/components/arc/arc.component.ts
+++ src/app/components/arc/arc.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-arc',
   templateUrl: './arc.component.html',
-  styleUrls: ['./arc.component.scss']
+  styleUrls: ['./arc.component.scss'],
 })
 export class ArcComponent implements OnInit {
+  @Input() critical: number = 10;
+  @Input() low: number = 15;
+  @Input() full: number = 10;
+  @Input() current: number = 50;
+  @Input() unit: string = 'cans';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get criticalDotValue(): string {
+    return this.updateCriticalDotValue();
   }
 
+  get lowDotValue(): string {
+    return `${(this.low * 100) / this.current}%`;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  updateCriticalDotValue(): string {
+    return `${(this.critical * 100) / this.current}%`;
+  }
 }
